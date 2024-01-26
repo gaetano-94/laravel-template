9bb1fea8307d5b8b3301e65c9c44f84d81b87a21
gaetano-94 <gaetano.panico1994@gmail.com>
2024-01-26 17:22:08 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import "./bootstrap";
+
+//inclusione del css nel progetto
+import "~resources/scss/app.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+//process images
+import.meta.glob(["../img/**"]);
